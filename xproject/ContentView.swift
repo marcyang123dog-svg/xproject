e8302d3a2813d1fadad297028979867d62506f51
marc <marc>
2026-03-17 14:10:29 +0800

    Initial Commit

---
+++ xproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xproject
+//
+//  Created by marc yaong on 2026/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xproject/xprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  xprojectApp.swift
+//  xproject
+//
+//  Created by marc yaong on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct xprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
